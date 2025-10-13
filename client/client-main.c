bfdf013d37d66292085ae2b88d467d6ba0bfc1e1
abmagarin <151558634+abmagarin@users.noreply.github.com>
2025-10-13 17:51:17 +0200

    client logic

---
+++ client/client-main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 1865
+#define BUFFER_SIZE 1024
+
+int main()
+{
+    int sock;
+    struct sockaddr_in server_addr;
+    char message[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("Error al crear el socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error al conectar con el servidor");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Conectado al servidor %s:%d\n", SERVER_IP, SERVER_PORT);
+
+    while (1)
+    {
+        printf("Introduce un mensaje ('SALIR' para terminar): ");
+        fgets(message, sizeof(message), stdin);
+
+        if (strncmp(message, "SALIR", 5) == 0)
+            break;
+
+        send(sock, message, strlen(message), 0);
+
+        int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received > 0)
+        {
+            buffer[bytes_received] = '\0';
+            printf("Respuesta del servidor: %s\n", buffer);
+        }
+    }
+
+    close(sock);
+    printf("Desconectado del servidor.\n");
+
+    return 0;
+}
